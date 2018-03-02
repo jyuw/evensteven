7959973de329805384fdf89ae44d99a6884e053e
bollen <david.boll@me.com>
2018-03-02 10:52:13 +0100

    Changes scope from "public_profile" to "email" in config/initializers/devise.rb Removes unnecessary step from feature-file Adds facebook logic to navbar Minor general beutifying of code

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,5 +12,5 @@ Devise.setup do |config|
   config.reset_password_within = 6.hours
   config.sign_out_via = :delete
   config.omniauth :facebook, ENV['FACEBOOK_OAUTH_KEY'], ENV['FACEBOOK_OAUTH_SECRET'],
-                  {scope: 'public_profile', callbackURL: 'auth/facebook/callback/'}
+                  {scope: 'email', callbackURL: 'auth/facebook/callback/'}
 end
