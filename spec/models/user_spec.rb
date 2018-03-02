38b7b1d7be499e892f42aec873d7f1b628321ae1
bollen <david.boll@me.com>
2018-03-02 10:55:13 +0100

    Adds test for checking fb-columns in DB-table users

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -13,7 +13,12 @@ RSpec.describe User, type: :model do
     it {is_expected.to have_db_column :email}
     it {is_expected.to have_db_column :encrypted_password}
   end
-  
+
+  describe 'db table with added fb-login' do
+    it {is_expected.to have_db_column :uid}
+    it {is_expected.to have_db_column :provider}
+  end
+
   describe 'Associations' do
     it {is_expected.to have_and_belong_to_many :groups}
     it {is_expected.to have_many :expenses}
