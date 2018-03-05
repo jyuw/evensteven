fd19421c29cd56ab39d06209fe0166ff6804149e
Jade <jywfang@gmail.com>
2018-03-05 09:52:05 +0100

    refactored space to make it prettier

--- spec/models/group_spec.rb
+++ spec/models/group_spec.rb
@@ -16,6 +16,6 @@ RSpec.describe Group, type: :model do
 
   describe 'Associations' do
     it {is_expected.to have_and_belong_to_many :users}
-    it {is_expected. to have_many :expenses}
+    it {is_expected.to have_many :expenses}
   end
 end
